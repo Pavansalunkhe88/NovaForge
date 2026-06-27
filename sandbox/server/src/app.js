dd894b0cc19bb5fed42814a46508c814c0dc1596
Pavan Salunkhe <pavansalunkhe17@gmail.com>
2026-06-27 19:34:12 +0530

    feat: initialize React app with Vite, add main components and styles
    - Created App component with counter functionality.
    - Added CSS styles for layout and components.
    - Included assets for logos and images.
    - Set up Vite configuration for development server.
    - Established index file for rendering the app.

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import {createPod} from "./kubernetes/pod.js";
+import {createService} from "./kubernetes/service.js";
+import { v7 as uuid} from "uuid";
 
 const app = express();
 
@@ -24,5 +27,22 @@ app.get("/api/sandbox", (req, res) => {
 app.get("/api/sandbox/health", (req, res) => {
     res.status(200).send("hello world sandbox");
 });
+
+app.post("/api/sandbox/start", async (req, res) => {
+    const sandboxId = uuid();
+
+    await Promise.all([
+        createPod(sandboxId),
+        createService(sandboxId)
+    ])
+
+    return res.status(200).json({
+        status: "success",
+        message: "Sandbox environment created successfully",
+        sandboxId: sandboxId,
+        previewUrl: `http://{sandboxId}.preview.localhost` // Assuming a local development environment
+    });
+})
+
 
 export default app;
